ce12fcb9149cb72b732ee05a811bacf87b2bc841
Vallencius <vallencius1901@gmail.com>
2022-08-02 17:51:03 +0700

    Javascript phone test

---
+++ js/phone.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+    if( isMobile.Android() ) alert('Android');
+    if( isMobile.iOS() ) alert('iOS');
+    if( isMobile.Windows() ) alert('Windows');
+})
