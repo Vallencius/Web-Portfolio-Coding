12c76b49c7c12046d8b81f8722d2a65843435cd5
Vallencius <vallencius.siswanto@student.umn.ac.id>
2024-02-08 21:38:43 +0700

    Fix audio and height on phone

--- js/audio.js
+++ js/audio.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', '../audio/main.mp3');
-    audioElement.play();
 
     $('.mute').click(function() {
         audioElement.play();
+        if (document.contains(document.getElementById("mainAudio"))) {
+            document.getElementById("mainAudio").remove();
+        }
     });
     
     $('.music-on').click(function() {
         audioElement.pause();
+        if (document.contains(document.getElementById("mainAudio"))) {
+            document.getElementById("mainAudio").remove();
+        }
     });
 });
